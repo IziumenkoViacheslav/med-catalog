0c034e1d82c6e72804d4cabbce0e1a8f50804e51
IziumenkoViacheslav <2744097@gmail.com>
2022-04-23 11:44:58 +0300

    fix link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className='text-3xl font-bold underline'>
       Hello world!
+      <Link href='/api/auth/login'>
+        <a>LOGIN</a>
+      </Link>
     </h1>
-  )
+  );
 }
