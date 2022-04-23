cd56ee1f2bf79aca354d03436a3a709de337853e
IziumenkoViacheslav <2744097@gmail.com>
2022-04-23 12:20:55 +0300

    profile added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import Link from 'next/link';
+import Profile from './profile';
 
 export default function Home() {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello world!
+    <>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       <Link href='/api/auth/login'>
         <a>LOGIN</a>
       </Link>
-    </h1>
+      <Link href='/api/auth/logout'>
+        <a>LOGOUT</a>
+      </Link>
+      <Profile/>
+    </>
   );
 }
