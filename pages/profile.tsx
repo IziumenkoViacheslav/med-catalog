3c2bb35ba9f94aa122dd02ecf2c0e391996d8306
IziumenkoViacheslav <2744097@gmail.com>
2022-04-23 12:22:22 +0300

    refactor

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,9 +1,7 @@
 import { useUser } from '@auth0/nextjs-auth0';
-import Link from 'next/link';
 
 export default function Profile() {
   const { user, isLoading } = useUser();
-console.log(user)
   return (
     <>
       <h1 className='text-3xl font-bold underline'>{user?.name}</h1>
